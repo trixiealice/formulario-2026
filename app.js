6f9df22ed008c995d1a8e6208b0c96c460d801b2
ALICE CAMPOS DA SILVA <25203690@cfp127aluno.local>
2026-04-28 16:44:30 -0300

    Subistituição do viacep pelo Brasil API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 async function getDadosCep(cep){
-    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const url = `https://brasilapi.com.br/api/cep/v1/${cep}`
     const response = await fetch(url)
     const data =  await response.json()
     return(data)
@@ -12,10 +12,10 @@ async function preencherFormulario(){
     const cep = document.getElementById('cep').value
     const dadosCep = await getDadosCep(cep)
 
-    document.getElementById('endereco').value   = dadosCep.logradouro
-    document.getElementById('bairro').value     = dadosCep.bairro
-    document.getElementById('cidade').value     = dadosCep.localidade
-    document.getElementById('estado').value     = dadosCep.uf
+    document.getElementById('endereco').value   = dadosCep.street
+    document.getElementById('bairro').value     = dadosCep.neighborhood
+    document.getElementById('cidade').value     = dadosCep.city
+    document.getElementById('estado').value     = dadosCep.state
 
 }
 
